b4129e46eb346ee57259fd530fbf8f7f2afef915
Thyke Adams <45541936+ThykeAdams@users.noreply.github.com>
2021-10-28 22:46:19 -0300

    Removed useless Indent

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
 console.log("Hey there, I see you peeking!");
-console.log(
-	"Feel free to check out all the code on my github: https://github.com/abbyfischler");
+console.log("Feel free to check out all the code on my github: https://github.com/abbyfischler");
